1e05882839e21fe0ce809c15cb513a6ea2f05b1f
afranioag <afranio.garcia@ccc.ufcg.edu.br>
2021-01-20 19:43:43 -0300

    classe categoria

---
+++ spring-boot-ionic_nelioAlves/projeto_do_curso/curso_rest/src/main/java/com/projetoNelioAlves/curso_rest/domain/Categoria.java
@@ -0,0 +1,53 @@
+package com.projetoNelioAlves.curso_rest.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String nome;
+
+    public Categoria(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return id == categoria.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
--- spring-boot-ionic_nelioAlves/projeto_do_curso/curso_rest/src/main/java/com/projetoNelioAlves/curso_rest/resources/CategoriaResource.java
+++ spring-boot-ionic_nelioAlves/projeto_do_curso/curso_rest/src/main/java/com/projetoNelioAlves/curso_rest/resources/CategoriaResource.java
@@ -1,16 +1,34 @@
 package com.projetoNelioAlves.curso_rest.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.projetoNelioAlves.curso_rest.domain.Categoria;
 import org.springframework.boot.autoconfigure.quartz.QuartzTransactionManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String teste(){
-        return "correu tudo bem por aqui!!";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2,"escritoire");
+
+        List<Categoria> umCu = new ArrayList<Categoria>();
+
+        umCu.add(cat1);
+        umCu.add(cat2);
+
+        return umCu;
+    }
+
+    @GetMapping("/api")
+    public String nome(){
+        return "maria";
     }
 }
